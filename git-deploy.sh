501265835e7511570e798a0b29d7b3cde6ce1457
Seung Ki Lee <seungkilee.cs@gmail.com>
2024-02-10 18:12:54 +0900

    Updating git deploy script-2024-02-10_18_12_46

--- git-deploy.sh
+++ git-deploy.sh
@@ -7,14 +7,30 @@ if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
     # Get the current timestamp
     timestamp=$(date +"%Y-%m-%d_%H_%M_%S")
 
-    # Create the branch name
-    branch_name="automated-backup-$timestamp"
+    # Prompt the user for a commit message
+    read -p "Enter commit message: " commit_message
+
+    # Set the commit message, appending the timestamp if empty
+    if [ -z "$commit_message" ]; then
+        commit_message="autodeploy-$timestamp"
+    else
+        commit_message="$commit_message-$timestamp"
+    fi
+
+    # Check if there are any new changes
+    if git diff-index --quiet HEAD --; then
+        echo "No new changes to commit. Exiting."
+        exit 1
+    fi
 
     # Add all changes to git
     git add .
 
     # Commit the changes
-    git commit -m "auto deploy script"
+    git commit -m "$commit_message"
+
+    # Create the branch name
+    branch_name="autodeploy-$timestamp"
 
     # Create a new branch and switch to it
     git checkout -b $branch_name
